d8a4edef536f67ef803af8ae3cc368f4c8a781b3
Ranime <156202462+Ranimev2@users.noreply.github.com>
2024-01-25 23:56:08 +0700

    Update AnimeSearch.tsx

--- src/pages/AnimeSearch.tsx
+++ src/pages/AnimeSearch.tsx
@@ -30,7 +30,7 @@ const AnimeSearch = () => {
     setAnimes([]);
     setSearchParams({ q: searchInput });
     const response = (
-      await axios.get(`https://api.jikan.moe/v4/anime?q=${searchInput}`)
+      await axios.get(`https://otakudesu-zeta.vercel.app/api/otakudesu/ongiong?q=${searchInput}`)
     ).data;
 
     const { data } = response;
