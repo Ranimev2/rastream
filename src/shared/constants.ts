e7fd9167c46725044e65fcffb8eb8490e811dfc2
Ranime <156202462+Ranimev2@users.noreply.github.com>
2024-01-26 06:26:04 +0700

    Update constants.ts

--- src/shared/constants.ts
+++ src/shared/constants.ts
@@ -3,7 +3,7 @@ export const IMAGE_URL = "https://image.tmdb.org/t/p";
 export const EMBED_TO = "https://www.2embed.to/embed/tmdb";
 
 
-export const ANIME_API_URL = "https://api-consumet-org-six.vercel.app/"
+export const ANIME_API_URL = "https://api.jiken.moe/v4/anime"
 
 export const SUPPORTED_QUERY = {
   genre: [],
